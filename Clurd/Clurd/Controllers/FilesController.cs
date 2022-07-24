b04dbb1b8ec4f7d383c340e2af13f202101ece3a
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2022-07-24 19:45:47 +0200

    add download of file

--- Clurd/Clurd/Controllers/FilesController.cs
+++ Clurd/Clurd/Controllers/FilesController.cs
@@ -9,4 +9,18 @@ public class FilesController : Controller
         var io = new IO.IO();
         return io.GetFiles(value["path"]);
     }
+
+    [HttpPost("api/file")]
+    public async Task<FileStreamResult?> GetFile(IFormCollection value){
+        try{
+            // Need to add password support
+            var file = await System.IO.File.ReadAllBytesAsync(value["path"]);
+            Stream stream = new MemoryStream(file);
+            return File(stream, "application/octet-stream");
+        }catch(Exception ex){
+            Console.WriteLine(ex);
+            return null;
+        }
+
+    }
 }
--- Clurd/Clurd/Scripts/lib/components/Contex/contex.svelte
+++ Clurd/Clurd/Scripts/lib/components/Contex/contex.svelte
@@ -2,10 +2,7 @@
 	export let current_file, path, pathsplitted;
 	let url;
 	import { dialogs } from 'svelte-dialogs';
-	// import { copy } from '../ts/copy';
 	import { onMount } from 'svelte';
-	// import FSBrowser from '../file-browser/browser.svelte';
-	// import Details from '$lib/details/details.svelte';
 	import { Rename, Delete } from '$lib/js/io.js';
     import Fa from 'svelte-fa/src/fa.svelte';
     import { faCopy } from '@fortawesome/free-solid-svg-icons/index.es';
@@ -38,9 +35,45 @@
 			}
 		});
 	}
+	function downloadfile(){
+		console.log(path);
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+		var urlencoded = new URLSearchParams();
+		urlencoded.append("path", path);
+
+		var requestOptions = {
+		method: 'POST',
+		headers: myHeaders,
+		body: urlencoded,
+		redirect: 'follow'
+		};
+
+		fetch("/api/file", requestOptions)
+		.then(function(resp) {
+			return resp.blob();
+		}).then(function(blob) {
+			// @ts-ignore it's loaded in head of page
+			return download(blob, current_file);
+		})
+		.catch(error => console.log('error', error));
+		
+		// fetch('/api/file', {
+		// 	method: 'POST',
+		// 	body: JSON.stringify({
+		// 		path: path + "/" +current_file
+		// 	}),
+		// 	headers: {
+		// 		'Content-Type': 'application/json'
+		// 	},
+		// })
+	}
 </script>
 
 <svelte:head>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/downloadjs/1.4.8/download.min.js"></script>
+
 	<link
 		rel="stylesheet"
 		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
@@ -51,19 +84,14 @@
 		<li class="rename" on:click={renamefile}>
 			<a href="#0" ><i class="fa fa-pencil" aria-hidden="true" /> Rename</a>
 		</li>
-		 <!-- <li class="link">
-			<a href="#0" on:click={copy(current_file)}
-				><i class="fa fa-link" aria-hidden="true" /> Copy Link Address</a
-			>
-		</li> -->
 		<li class="copy" on:click={() => dialogs.modal(FileBrowser, {old_path: path, current_file: current_file})}>
 			<a href="#0">
 				<i class="fa fa-copy" aria-hidden="true" /> Copy/Move to
 			</a>
 		</li>
-		<!--<li class="download">
-			<a href={current_file}><i class="fa fa-download" aria-hidden="true" /> Download</a>
-		</li>-->
+		<li class="download">
+			<a on:click={downloadfile}><i class="fa fa-download" aria-hidden="true" /> Download</a>
+		</li>
 		<li
 			class="trash"
 			on:click={remove}
