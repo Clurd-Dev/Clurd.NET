766effd84207234fc1a9d111ee5dae6e42a1f68d
Andrea055 <vivanco11.ac@gmail.com>
2022-07-26 22:56:20 +0200

    add linux installer

---
+++ Clurd/Clurd/LinuxInstaller/clurd.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+### BEGIN INIT INFO
+# Provides:          Clurd
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Clurd server
+# Description:       Run Clurd server
+### END INIT INFO
+
+# Carry out specific functions when asked to by the system
+case "$1" in
+  start)
+    echo "Starting Clurd server"
+    sudo -u USER bash -c 'cd PATHTOCLURD & nohup ./Clurd &> log.out &'
+    ;;
+  stop)
+    echo "Stopping Clurd server"
+    sudo -u USER bash -c 'fuser -k 5000/tcp'
+    sleep 2
+    ;;
+  *)
+    echo "Usage: /etc/init.d/clurd {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
---
+++ Clurd/Clurd/LinuxInstaller/install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/Clurd-Dev/Clurd.NET.git
+cd Clurd.NET/Clurd/Clurd/
+npm i
+dotnet publish --configuration Release
+cd bin/Release/net6.0/publish
